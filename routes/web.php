ee111c4d1af64b6b7c187cacdd65142a6901f91b
DhyasR <119585072+DhyasR@users.noreply.github.com>
2025-01-07 12:14:04 +0700

    update code

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,16 @@
 use App\Http\Controllers\ReciptMakerController;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
 
 Route::post('/generate/send', [ReciptMakerController::class, 'sendMessage'])->withoutMiddleware(VerifyCsrfToken::class);
 Route::get('/generate/messages', [ReciptMakerController::class, 'getMessages'])->withoutMiddleware(VerifyCsrfToken::class);
-Route::get('/api-key', function () {
-    return response()->json(['api_key' => env('OPENAI_API_KEY')]);
-});
+// Route::get('/createToken', function(){
+//    $user = User::create([
+//     'name' => 'Galih',
+//     'email'=> 'aahjaaa@gmail.com',
+//     'password'=> bcrypt('password')
+//    ]);
+
+//    return    $user->createToken('apiKey')->plainTextToken;
+// });
